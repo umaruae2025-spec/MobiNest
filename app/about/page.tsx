214faafe887ef8ddc7b32c8058c56d7eb83cbd00
MacBook <newtouch@MacBooks-MacBook-Pro.local>
2026-06-30 10:57:28 +0400

    fix home page layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,18 +1,24 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main style={{ padding: "40px", textAlign: "center" }}>
-      <h1>MobiNest 🚀</h1>
-      <p>Your modern web solution platform</p>
+    <>
+      <Navbar />
 
-      <button style={{
-        marginTop: "20px",
-        padding: "10px 20px",
-        background: "black",
-        color: "white",
-        borderRadius: "8px"
-      }}>
-        Get Started
-      </button>
-    </main>
+      <main style={{ padding: "40px", textAlign: "center" }}>
+        <h1>MobiNest 🚀</h1>
+        <p>Your modern web solution platform</p>
+
+        <button style={{
+          marginTop: "20px",
+          padding: "10px 20px",
+          background: "black",
+          color: "white",
+          borderRadius: "8px"
+        }}>
+          Get Started
+        </button>
+      </main>
+    </>
   );
 }
