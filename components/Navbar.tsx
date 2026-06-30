8c0e53adc5dd6a6ed775e2bc279e2745aa3fd35c
MacBook <newtouch@MacBooks-MacBook-Pro.local>
2026-06-30 09:54:34 +0400

    fix navbar styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,14 +7,15 @@ export default function Navbar() {
       justifyContent: "space-between",
       padding: "15px 30px",
       background: "black",
-      color: "white"
+      color: "white",
+      alignItems: "center"
     }}>
-      <h2>MobiNest</h2>
+      <h2 style={{ color: "white" }}>MobiNest</h2>
 
       <div style={{ display: "flex", gap: "15px" }}>
-        <Link href="/" style={{ color: "white" }}>Home</Link>
-        <Link href="/about" style={{ color: "white" }}>About</Link>
-        <Link href="/contact" style={{ color: "white" }}>Contact</Link>
+        <Link href="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
+        <Link href="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
+        <Link href="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link>
       </div>
     </nav>
   );
